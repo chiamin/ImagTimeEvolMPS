4806defec65154a3923f3c92220f6bb2889737c7
chiamin <chiaminchung@gmail.com>
2025-04-11 12:01:49 +0800

    Stabilize the determinants

--- ImagMPS.jl
+++ ImagMPS.jl
@@ -7,6 +7,7 @@ include("SampleDet.jl")
 include("Hamiltonian.jl")
 include("dmrg.jl")
 include("SampleMPSDet.jl")
+include("Initial.jl")
 
 function getED0(L, pbc, t, U, dtau, Ntau, U0)
     psi, E0, Hk, HV, H = ED_GS(L, pbc, t, U0)
@@ -34,7 +35,7 @@ function getEkEV(mps, Lx, Ly, tx, ty, U, xpbc, ypbc)
     return Ek, EV
 end
 
-function run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, psi)
+function run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, mps, write_step, stblz_step)
     Nsites = Lx*Ly
     Npar = Nup+Ndn
     tau = dtau * nsteps
@@ -59,30 +60,32 @@ function run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, p
     obs["Hk"] = Hk
     obs["U"] = U
 
-    # Initialize MPS
-    ppsi = makeProdMPS(psi)
+    # Initialize MPS machine which is efficient in computing the overlap with a product state
+    mpsM = makeProdMPS(mps)
 
     det_time = 0.
     mps_time = 0.
     mea_time = 0.
 
     dir = "data/"
-    file = open(dir*"/en"*string(nsteps)*".dat","w")
-    file_nup = open(dir*"/nup"*string(nsteps)*".dat","w")
-    file_ndn = open(dir*"/ndn"*string(nsteps)*".dat","w")
+    file = open(dir*"/ntau"*string(nsteps)*".dat","w")
+    # Write the observables' names
+    println(file,"step Ek EV sign nup ndn")
 
     # Monte Carlo sampling
     latt = makeSquareLattice(Lx, Ly, xpbc, ypbc)
     for i=1:N_samples
         # Sample the left product state
-        mps_t = @elapsed conf_beg, wMPS1 = sampleMPS!(conf_beg, ppsi, phis_up[1], phis_dn[1], latt)
+        # wMPS1: the weight from the MPS1
+        mps_t = @elapsed conf_beg, wMPS1 = sampleMPS!(conf_beg, mpsM, phis_up[1], phis_dn[1], latt)
         phis_up[0], phis_dn[0] = prodDetUpDn(conf_beg)
 
         # Sample the auxiliary fields from left to right
         det_t = @elapsed wDet = sampleAuxField_sweep!(phis_up, phis_dn, auxflds, expHk_half, expV_up, expV_dn; toRight=true)
 
         # Sample the right product state
-        mps_t = @elapsed conf_end, wMPS2 = sampleMPS!(conf_end, ppsi, phis_up[Ntau], phis_dn[Ntau], latt)
+        # wMPS2: the weight from the MPS2
+        mps_t = @elapsed conf_end, wMPS2 = sampleMPS!(conf_end, mpsM, phis_up[Ntau], phis_dn[Ntau], latt)
         phis_up[Ntau+1], phis_dn[Ntau+1] = prodDetUpDn(conf_end)
 
         # Sample the auxiliary fields from right to left
@@ -96,31 +99,29 @@ function run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, p
         mps_time += mps_t
         mea_time += mea_t
 
-        println(join(conf_beg," ")," ",join(conf_end," "),"  ",w)
-
-
-        if i%100 == 0
-            println(keys(obs))
+        # Write the observables
+        if i%write_step == 0
             Eki = getObs(obs, "Ek")
             EVi = getObs(obs, "EV")
             nupi = getObs(obs, "nup")
             ndni = getObs(obs, "ndn")
             si = getObs(obs, "sign")
 
-            println(file,i," ",Eki," ",EVi," ",si)
-            println(file_nup,join(nupi, " "))
-            println(file_ndn,join(ndni, " "))
+            println(file,i," ",Eki," ",EVi," ",si," ",nupi," ",ndni)
             flush(file)
-            flush(file_nup)
-            flush(file_ndn)
 
             cleanObs(obs)
+        end
 
-            println("time ",det_t," ",mps_t," ",mea_t)
+        # Stabilize the determinants
+        if i%stblz_step == 0
+            stabilizeDet!(phis_up, phis_dn)
         end
     end
     close(file)
-    println("total time ",det_time," ",mps_time," ",mea_time)
+    println("total time: ")
+    println(rpad("det",8),rpad("mps",8),rpad("mea",8))
+    println(rpad(round(det_time,digits=3),8),rpad(round(mps_time,digits=3),8),rpad(round(mea_time,digits=3),8))
 end
 
 function measureMPS(psi)
@@ -142,7 +143,9 @@ function main()
     U = 12.
     dtau = 0.05
     nsteps = 10
-    N_samples = 300
+    N_samples = 30
+    write_step = 10
+    stblz_step = 10
 
     # Initialize MPS
     en0, psi = Hubbard_GS(Lx, Ly, tx, ty, U, xpbc, ypbc, Nup, Ndn; nsweeps=10, maxdim=[10], cutoff=[1e-14])
@@ -168,8 +171,8 @@ function main()
         println(file,"ndn ",nups)
     end
 
-    for nsteps in [10]#,20,30,40,50,60,70,80]
-        run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, psi)
+    for nsteps in [10,20,30]
+        run(Lx, Ly, tx, ty, xpbc, ypbc, Nup, Ndn, U, dtau, nsteps, N_samples, psi, write_step, stblz_step)
     end
 end
 
